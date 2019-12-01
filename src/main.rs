082833b89b224e7801e63fbb6dc7cc2f04220e68
Larry850806 <pudding850806@gmail.com>
2019-12-01 16:43:48 +0800

    Finish benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-#![cfg_attr(test, feature(test))]
-
-#[cfg(test)]
-extern crate test;
+use std::time::Instant;
 
 fn double(vec: &Vec<i32>) -> Vec<i32> {
     let mut vec2 = Vec::new();
@@ -11,31 +8,43 @@ fn double(vec: &Vec<i32>) -> Vec<i32> {
     return vec2;
 }
 
+fn double2(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec2 = Vec::with_capacity(vec.len());
+    for x in vec.iter() {
+        vec2.push(x * 2);
+    }
+    return vec2;
+}
+
 fn main() {
-    let vec = vec![1, 2, 3, 4, 5, 6, 7];
+    let mut vec = Vec::new();
 
-    let vec2 = double(&vec);
-
-    println!("{:?}", vec);
-    println!("{:?}", vec2);
-}
-
-pub fn add_two(a: i32) -> i32 {
-    a + 2
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use test::Bencher;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(4, add_two(2));
+    for x in 0..100000 {
+        vec.push(x);
     }
 
-    #[bench]
-    fn bench_add_two(b: &mut Bencher) {
-        b.iter(|| add_two(2));
+    let mut total_time: u128 = 0;
+    {
+        for _ in 0..1000 {
+            let now = Instant::now();
+            double(&vec);
+            total_time += now.elapsed().as_micros();
+        }
+        println!("double:  {}μs for 1000 times", total_time);
     }
+
+    let mut total_time2: u128 = 0;
+    {
+        for _ in 0..1000 {
+            let now = Instant::now();
+            double2(&vec);
+            total_time2 += now.elapsed().as_micros();
+        }
+        println!("double2: {}μs for 1000 times", total_time2);
+    }
+
+    println!(
+        "double2 is {:.2} times faster than double",
+        total_time as f64 / total_time2 as f64
+    );
 }
